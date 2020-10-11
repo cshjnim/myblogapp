146d49514041003e9c00fff905c93e1bc92e00ac
Hyejin Kim <cshjkim@gmail.com>
2020-10-10 17:58:10 -0700

    updating the footer, grid, jumbotron, and nav

---
+++ myblogapp/src/components/Footer/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../Footer/style.css"
+
+function Footer() {
+  return (
+    <footer>
+      <hr />
+      <p className="pull-right">
+        <i className="fab fa-github" /> copyright @Hyejin Kim
+      </p>
+    </footer>
+  );
+}
+
+export default Footer;
